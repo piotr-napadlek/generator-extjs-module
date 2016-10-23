c902fe8d64764473a0c45586d030d2c636828541
piotr-napadlek <fisheye@gazeta.pl>
2016-10-23 12:11:25 +0200

    partial logic in index.js

--- generators/app/index.js
+++ generators/app/index.js
@@ -1,20 +1,43 @@
 'use strict';
 var yeoman = require('yeoman-generator');
-var chalk = require('chalk');
-var yosay = require('yosay');
+var json = require('comment-json');
 
 module.exports = yeoman.Base.extend({
   prompting: function () {
-    // Have Yeoman greet the user.
-    this.log(yosay(
-      'Welcome to the super-duper ' + chalk.red('generator-extjs-module') + ' generator!'
-    ));
+    var lvlsUp = 0;
+    var appJson = function (me) {
+      var scannedFolder = me.destinationRoot();
+      for (var i = 0; i <= 50; i++) { // as we don't want to load many deps lets just scan 50 levels up
+        if (me.fs.exists(scannedFolder + '\\app.json')) {
+          lvlsUp = i - 1;
+          return json.parse(me.fs.read(scannedFolder + '\\app.json'), null, true);
+        }
+        scannedFolder = scannedFolder + '\\..';
+      }
+      me.env.error('Was searching painstakingly for an app.json file defining important app config, but didn\'t find any. Sorry, I cannot continue :c');
+    };
+    var appConfig = appJson(this);
+    var packageName = (function (me) {
+      var pathRoot = me.destinationRoot();
+      pathRoot = pathRoot.replace('\\', '.');
+      pathRoot = pathRoot.replace('\\\\', '.');
+      pathRoot = pathRoot.replace('/', '.');
+      var packages = pathRoot.split('.');
+      if (lvlsUp < 0) {
+        packages = [];
+      } else {
+        packages.slice(-1 * lvlsUp);
+      }
+      me.log(packages);
+    })(this);
 
+    this.log('Welcome to ExtJS module generator. Report any bugs at piotr.napadlek@gmail.com');
+    this.log('Please provide some info to scaffold ready-to-go module!');
     var prompts = [{
-      type: 'confirm',
-      name: 'someAnswer',
-      message: 'Would you like to enable this option?',
-      default: true
+      type: 'input',
+      name: 'moduleName',
+      message: 'What is the name of the module? Please use camelcase, as you would declare ExtJS class name. It will be converted automatically.\n' +
+      'Eg. \'NewModule\' will create a module folder named \'newmodule\' with a view directory containing file \'NewModule.js\''
     }];
 
     return this.prompt(prompts).then(function (props) {
@@ -24,13 +47,13 @@ module.exports = yeoman.Base.extend({
   },
 
   writing: function () {
-    this.fs.copy(
-      this.templatePath('dummyfile.txt'),
-      this.destinationPath('dummyfile.txt')
-    );
+    // this.fs.copy(
+    //   this.templatePath('dummyfile.txt'),
+    //   this.destinationPath('dummyfile.txt')
+    // );
   },
 
   install: function () {
-    this.installDependencies();
+    //this.installDependencies();
   }
 });
